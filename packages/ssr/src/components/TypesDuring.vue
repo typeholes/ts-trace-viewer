f1244ae8da60183f091264c1ad8e0a93ebd585ff
typeholes <typeholes@protonmail.net>
2024-05-10 22:54:37 -0400

    commented out type info for now (perf)

--- packages/ssr/src/components/TypesDuring.vue
+++ packages/ssr/src/components/TypesDuring.vue
@@ -16,18 +16,20 @@ const types = computed(
 
 <template>
   <q-card class="q-pl-md">
+      <!-- <q-expansion-item label="types"> -->
     <div>
-      Types created: {{ types.length }}
-      <div class="q-pl-md" v-for="type in types" :key="type.id">
-        <div class="row justify-between">
-          <div class="col-1">ts: {{ Math.round(type.ts ?? 0) }}</div>
-          <div class="col-1">dur: {{ Math.round(type.dur ?? 0) }}</div>
-          <div class="col-1">id: {{ type.id }}</div>
-          <div class="col-1">{{ type.recursionId }}</div>
-          <div class="col-8">{{ type.flags }}</div>
-        </div>
-        <div class="q-pl-md" v-if="type.display">{{ type.display }}</div>
-      </div>
+          Types created: {{ types.length }}
+        <!-- <div class="q-pl-md" v-for="type in types" :key="type.id">
+          <div class="row justify-between">
+            <div class="col-1">ts: {{ Math.round(type.ts ?? 0) }}</div>
+            <div class="col-1">dur: {{ Math.round(type.dur ?? 0) }}</div>
+            <div class="col-1">id: {{ type.id }}</div>
+            <div class="col-1">{{ type.recursionId }}</div>
+            <div class="col-8">{{ type.flags }}</div>
+          </div>
+          <div class="q-pl-md" v-if="type.display">{{ type.display }}</div>
+        </div> -->
     </div>
+      <!-- </q-expansion-item> -->
   </q-card>
 </template>
